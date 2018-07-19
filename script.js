8f028cf1f74d45774d766b2f621c16ab64db139e
naijaaa <41436754+naijaaa@users.noreply.github.com>
2018-07-19 01:45:26 -1200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function popup(){
+  alert("Hello!");
+  alert("Welcome to my page, enjoy learning about me");
+
+}
+
+function prank(){
+  // <img id="glizzy"
+  // src="https://pbs.twimg.com/media/DP7YOUlUMAA86M_.jpg";
+  document.getElementById('ybn2').src="https://www.billboard.com/files/styles/article_main_image/public/media/YBN-namir-2017-cr-Cheryl-Fox-billboard-1548.jpg";
+// <img src="ybn2" onclick="this.src='glizzy">
+}
+function haha(){
+  document.getElementById('ybn2').src="https://www.ascap.com/~/media/site-pages/news-and-events/articles/2018/04/ybn_nahmir_1.jpg?la=en&hash=BFE836897FBA6EF7F2B2B825A43E8B5F77070CF1";
+
+}
